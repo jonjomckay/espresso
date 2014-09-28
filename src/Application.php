ed1e2bc094f39a2f8fd31cdacc81ce348c70be8d
Jonjo McKay <jonjo@jonjomckay.com>
2014-09-28 17:39:05 +0100

    Made compatible with Silex controllers

--- src/Application.php
+++ src/Application.php
@@ -5,6 +5,7 @@ namespace React\Espresso;
 use React\Http\Request;
 use React\Http\Response;
 use Silex\Application as BaseApplication;
+use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
@@ -24,7 +25,11 @@ class Application extends BaseApplication
     public function __invoke(Request $request, Response $response)
     {
         $sfRequest = $this->buildSymfonyRequest($request, $response);
-        $this->handle($sfRequest, HttpKernelInterface::MASTER_REQUEST, false);
+
+        $result = $this->handle($sfRequest, HttpKernelInterface::MASTER_REQUEST, false);
+
+        $response->writeHead($result->getStatusCode(), $result->headers->all());
+        $response->end($result->getContent());
     }
 
     private function buildSymfonyRequest(Request $request, Response $response)
